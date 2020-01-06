84104bbd5c0bb5378f425cc212eaf292a2bbdea4
vijay kasina <vijaya_kasina@gap.com>
2020-01-05 20:40:44 -0500

    code cleanup

--- src/main/java/com/oracle/familytree/dto/RelationDTO.java
+++ src/main/java/com/oracle/familytree/dto/RelationDTO.java
@@ -1,12 +1,16 @@
 package com.oracle.familytree.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RelationDTO {
 
 	Long personId;
